dd55111b9fd3bcb8fee123fe0ab74835c8ed2b48
Freshell-Da Silva <istlfdasilvafreshell@gmail.com>
2026-06-19 08:21:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 50){
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+
+});
+
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+cards.forEach(card=>{
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(50px)";
+    card.style.transition = ".8s";
+
+    observer.observe(card);
+
+});
